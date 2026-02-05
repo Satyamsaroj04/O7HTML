a2652f07c938805423e31757c81230f624d0d2a8
Satyamsaroj04 <satyam2671211@gmail.com>
2026-02-05 11:16:49 +0530

    cinevue

---
+++ js/firstday.js
@@ -0,0 +1,19 @@
+//  person name
+let person_name = "Satyam";
+console.log("My name is:", person_name);
+
+// Two number variables
+let num1 = 10;
+let num2 = 5;
+
+// Addition of two numbers
+console.log("Addition:-", num1 + num2);
+
+// Subtraction of two numbers
+console.log("Subtraction:-", num1 - num2);
+
+// Multiplication of two numbers
+console.log("Multiplication:-", num1 * num2);
+
+// Division of two numbers
+console.log("Division:-", num1 / num2);
